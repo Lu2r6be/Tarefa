c5df2d29e05cb5c9f777eeb335cbaa776d9b9051
Luiz Henrique <lugrabe26@gmail.com>
2024-05-02 11:21:47 -0300

    Criando a função remover

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,9 @@ function listarTarefas(){
     }
     document.getElementById("lista").innerHTML =valor;
 }
+//Função Remover Tarefas
+
+function removerTarefa(){
+    tarefas.pop();
+    listarTarefas();
+}
