fabc976fd236e456422235146c0641198a0f515f
Luiz Henrique <lugrabe26@gmail.com>
2024-05-02 11:03:19 -0300

    Criando a função adicionarTarefa

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,23 @@ function validaCampo(){
     valida = true;
     return valida;
 }
+
+//Função que adiciona a tarefa
+
+function adicionarTarefa(){
+    //Criando a variavel que vai receber a tarefas
+    let linhas = document.getElementById('task');
+    if(validaCampo()){
+        alert('Preencha o campo com a tarefa')
+    }else{
+        //Adicionar as tarefas
+        tarefas.push(linha.value);
+        //Passando para a proxima linha vazia
+        linha.value='';
+        //Chamando a função que vai mostrar todas as tarefas
+        listarTarefas();
+    }
+    //Volta para a caixa de input
+    document.getElementById('task').focus();
+    
+}
